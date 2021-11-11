9d1daae6f4f6ae53fcc7ccdcc19999c366582d7c
Fam0r <fam0r@mailbox.org>
2021-11-11 21:11:21 +0200

    Hello Lynn5

--- __main__.py
+++ __main__.py
@@ -1,24 +1,8 @@
-import glob
-import logging
 import os
 
 import hikari
-import lightbulb
 
 import lynn
-
-
-def create_bot() -> lynn.Bot:
-    """Create and return bot"""
-    bot = lynn.Bot(intents=hikari.Intents.ALL)
-
-    for extension in [f.replace('.py', '').replace('/', '.').replace('\\', '.') for f in glob.glob('extensions/**/*.py', recursive=True)]:
-        try:
-            bot.load_extension(extension)
-        except lightbulb.errors.ExtensionError as e:
-            logging.error('Failed to load extension: %s', e)
-
-    return bot
 
 if __name__ == '__main__':
     # uvloops improves performance, but is only available for UNIX systems.
@@ -26,5 +10,5 @@ if __name__ == '__main__':
         import uvloop
         uvloop.install()
 
-    bot = create_bot()
+    bot = lynn.Bot()
     bot.run(activity=bot.config.get('activity'), status=bot.config.get('status'))
--- lynn.py
+++ lynn.py
@@ -6,9 +6,7 @@ import sys
 import typing
 
 import hikari
-from hikari import undefined
 import lavasnek_rs
-import lightbulb
 
 
 class Config:
@@ -64,7 +62,8 @@ class Config:
             raise Error('This command is currently disabled.', 'Secret key missing, please contact the bot owner.')
         return secret
 
-class Error(lightbulb.errors.CommandError):
+
+class Error(Exception):
     """Custom error message, raise to send an error embed."""
 
     def __init__(self, title: str = None, text: str = None):
@@ -73,88 +72,22 @@ class Error(lightbulb.errors.CommandError):
         self.text: str = text
         """The error text."""
 
-class Response:
-    """Better response handling"""
-
-    def __init__(self, content: undefined.UndefinedOr[typing.Any] = undefined.UNDEFINED,
-        *,
-        attachment: undefined.UndefinedOr[hikari.files.Resourceish] = undefined.UNDEFINED,
-        attachments: undefined.UndefinedOr[typing.Sequence[hikari.files.Resourceish]] = undefined.UNDEFINED,
-        component = undefined.UNDEFINED,
-        components = undefined.UNDEFINED,
-        embed: undefined.UndefinedOr[hikari.Embed] = undefined.UNDEFINED,
-        embeds: undefined.UndefinedOr[typing.Sequence[hikari.Embed]] = undefined.UNDEFINED,
-        nonce: undefined.UndefinedOr[str] = undefined.UNDEFINED,
-        tts: undefined.UndefinedOr[bool] = undefined.UNDEFINED,
-        reply: typing.Union[
-            undefined.UndefinedType, hikari.snowflakes.SnowflakeishOr[hikari.PartialMessage], bool
-        ] = undefined.UNDEFINED,
-        mentions_everyone: undefined.UndefinedOr[bool] = undefined.UNDEFINED,
-        mentions_reply: undefined.UndefinedOr[bool] = undefined.UNDEFINED,
-        user_mentions: undefined.UndefinedOr[
-            typing.Union[hikari.snowflakes.SnowflakeishSequence[hikari.PartialUser], bool]
-        ] = undefined.UNDEFINED,
-        role_mentions: undefined.UndefinedOr[
-            typing.Union[hikari.snowflakes.SnowflakeishSequence[hikari.PartialRole], bool]
-        ] = undefined.UNDEFINED,):
-        self.content = content
-        self.force_disable_reply = False
-        if reply is False:
-            self.force_disable_reply = True
-        self.data = {
-            'attachment': attachment, 'attachments': attachments, 'component': component, 'components': components,
-            'embed': embed, 'embeds': embeds, 'nonce': nonce, 'tts': tts, 'mentions_everyone': mentions_everyone,
-            'mentions_reply': mentions_reply, 'user_mentions': user_mentions, 'role_mentions': role_mentions
-        }
-
-    async def send_channel(self, channel: hikari.SnowflakeishOr[hikari.TextableChannel], author_id: typing.Union[str, int, hikari.Snowflakeish]) -> hikari.Message:
-        raise NotImplementedError()
-        """
-        try:
-            return await self.bot.rest.create_message(content=self.content, channel=channel, **self.data)
-        except hikari.BadRequestError as e:
-            try:
-                # pylint: disable=unsubscriptable-object
-                if len(e.errors['message_reference']['_errors']) == 1 and e.errors['message_reference']['_errors'][0]['code'] == 'REPLIES_UNKNOWN_MESSAGE':
-                    msg = self.content
-                    if author_id:
-                        if msg:
-                            msg.append(f'<@{author_id}>, {self.content}')
-                        else:
-                            msg = f'<@{author_id}>'
-                    return await self.bot.rest.create_message(content=msg, channel=channel, **self.data)
-            except KeyError:
-                pass
-            return None"""
-
-    async def send(self, ctx: lightbulb.Context) -> hikari.Message:
-        if not self.force_disable_reply:
-            self.data['reply'] = ctx.message
-        #return await self.send_channel(ctx.channel_id, ctx.author.id)
-        return await ctx.respond(content=self.content, **self.data)
-
 class Data:
     """Global data shared across the entire bot."""
     def __init__(self) -> None:
         self.lavalink: lavasnek_rs.Lavalink = None # pylint: disable=no-member
 
-class Bot(lightbulb.Bot):
-    """Improved Bot class"""
+class Bot(hikari.GatewayBot):
+    """Bot class"""
     def __init__(self, *args, **kwargs):
         self.data = Data()
         self.config = Config()
         self.startup_time = time.time()
 
         self.token = self.config.get_secret('discord_token')
-        self.prefix = lightbulb.when_mentioned_or(self.config.get('prefix'))
+        self.prefix = '%'
 
-        super().__init__(token=self.token, prefix=self.prefix, *args, **kwargs)
-
-class Plugin(lightbulb.Plugin):
-    """Improved Plugin class"""
-    def __init__(self, bot: Bot):
-        super().__init__()
-        self.bot = bot
+        super().__init__(token=self.token, *args, **kwargs)
 
 ERROR_COLOR = 0xff4444
 EMBED_COLOR = 0x8f8f8f
