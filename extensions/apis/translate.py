6473a738836d0c804c74b5100fefe85ed4b31e99
Fam0r <fam0r@mailbox.org>
2022-03-03 15:30:16 +0200

    WIP translate command

--- extensions/apis/translate.py
+++ extensions/apis/translate.py
@@ -2,7 +2,7 @@ import datetime
 import hikari
 import lightbulb
 import lynn
-from helpers import rest, escape_url
+from helpers import rest, RestOptions
 
 @lightbulb.option('query', 'Sentence to translate', modifier=lightbulb.commands.OptionModifier.CONSUME_REST)
 @lightbulb.command('translate', 'これは英語で何を言っているのだろうか？', aliases=['trans'], auto_defer=True)
@@ -28,7 +28,7 @@ async def translate(ctx: lightbulb.Context):
     query = ' '.join(newquery)
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
-    data = await helpers.rest(f'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&ie=UTF-8&oe=UTF-8&sl={inlang}&tl={outlang}&q={query}', headers=headers)
+    data = await rest(f'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&ie=UTF-8&oe=UTF-8&sl={inlang}&tl={outlang}&q={query}', RestOptions(headers=headers))
     if not data:
         await ctx.reply('Did not get a response from Google. Probably an invalid language.')
         return
@@ -40,7 +40,9 @@ async def translate(ctx: lightbulb.Context):
         confidence = f'(confidence: {round(data[6]*100)}%)'
 
 
-    embed = hikari.Embed(title='WIP', description='use >text for now')
+    embed = hikari.Embed(title='Google Translate', description=confidence)
+    embed.add_field(f'From `{fromlang}`', query)
+    embed.add_field(f'To `{outlang}`', data[0][0][0])
     return lynn.Message(content=f'{fromlang} -> {outlang}: {data[0][0][0]} {confidence}',
                         embed=embed, output=out)
 
